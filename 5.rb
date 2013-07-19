a19a216da6bc98a2ff250519873e06f90d6637a2
John Musgrave <musgravejw@gmail.com>
2013-07-18 22:46:24 -0400

    solution to #5

---
+++ 5.rb
@@ -0,0 +1,21 @@
+test = 1
+solution = 0
+
+def is_divisible(n)
+	for i in 1...20
+		if n % i != 0
+			return false
+		end
+	end
+	return true
+end
+
+while solution == 0
+	if is_divisible(test)
+		solution = test
+	else
+		test += 1
+	end	
+end
+
+puts solution

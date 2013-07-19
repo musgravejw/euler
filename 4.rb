44f18a2cca891252fc55d5bd3af1c0343acd382f
John Musgrave <musgravejw@gmail.com>
2013-07-18 22:36:22 -0400

    solution to #4

---
+++ 4.rb
@@ -0,0 +1,22 @@
+previous = 0
+
+def is_palindrome(n)
+	for i in 0...(n.length / 2)
+		if n[i] != n[(n.length - 1) - i]
+			return false
+		end
+	end
+	return true
+end
+
+for n in 100...999
+	for i in 100...999
+		j = n*i
+		if is_palindrome(j.to_s) && j > previous
+			previous = j
+		end
+	end
+end
+
+puts previous
+

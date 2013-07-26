4e5d28812fb5c7ffc6d91f906878015e0fc3606c
John Musgrave <musgravejw@gmail.com>
2013-07-26 18:46:31 -0400

    #20

---
+++ 20.rb
@@ -0,0 +1,20 @@
+n = 0
+sum = 0
+
+def factorial(n)
+	total = 1
+	for i in 1..n
+		total *= i
+	end
+	return total
+end
+
+n = factorial(100)
+
+while n > 0
+	i = n % 10
+	sum += i
+	n /= 10
+end
+
+puts sum

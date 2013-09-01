4f0b847571a4eb8f36b465a917c1be6fc47e7cc5
John Musgrave <musgravejw@gmail.com>
2013-09-01 19:40:35 -0400

    solution to 23

--- 25/23.rb
+++ 25/23.rb
@@ -5,43 +5,43 @@ sum = 0
 a = []
 
 def abundant(n)
-	total = 0
-	result = false
-	for i in 1...n
-		if n % i == 0
-			total += i
-		end
-	end
-	if total > n
-		result = true
-	end
-	return result
+  total = 0
+  result = false
+  for i in 1...n
+    if n % i == 0
+      total += i
+    end
+  end
+  if total > n
+    result = true
+  end
+  return result
 end
 
 
 def sum_of_abundant(n, a)
-	result = false
-	for i in 0..a.size
-		for j in 0..a.size
-			unless a[i].nil? || a[j].nil?
-				if a[i] + a[j] == n					
-					return true
-				end
-			end
-		end
-	end
-	return result
+  result = false
+  for i in 0..a.size
+    for j in 0..a.size
+      unless a[i].nil? || a[j].nil?
+        if a[i] + a[j] == n					
+          return true
+        end
+      end
+    end
+  end
+  return result
 end
 
 
 for i in 1..28123	
-	if !sum_of_abundant(i, a)
-		sum += i
-	end
-	
-	if abundant(i)
-		a.push i
-	end	
+  if !sum_of_abundant(i, a)
+    sum += i
+  end
+
+  if abundant(i)
+    a.push i
+  end	
 end
 
 puts sum

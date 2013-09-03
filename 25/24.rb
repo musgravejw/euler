fc98c6404f651a5f7a4eb5d01b9272b00cf31cf2
John Musgrave <musgravejw@gmail.com>
2013-09-03 08:26:44 -0400

    solution to 24:

--- 25/24.rb
+++ 25/24.rb
@@ -13,21 +13,23 @@ def lex
               for g in 0..9
                 for h in 0..9
                   for i in 0..9
-                    str = "#{a}#{b}#{c}#{d}#{e}#{f}#{g}#{h}#{i}"
-                    temp = [a, b, c, d, e, f, g, h, i]
-                    puts str
-					
-                    if temp.uniq.length == temp.length
-                      if !set.include? str.to_i
-                        set.push str.to_i
-                        n += 1
-                      end
-                    end
-				  
-                    if n == 1000000
+				    for a in 0..9
+                      str = "#{a}#{b}#{c}#{d}#{e}#{f}#{g}#{h}#{i}"
+                      temp = [a, b, c, d, e, f, g, h, i]
                       puts str
-                      return str				      
-                    end
+					
+                      if temp.uniq.length == temp.length
+                        if !set.include? str.to_i
+                          set.push str.to_i
+                          n += 1
+                        end
+                      end
+				  
+                      if n == 1000000
+                        puts str
+                        return str				      
+                      end
+					end
                   end
                 end
               end
